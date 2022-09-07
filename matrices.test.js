f83d6bbf6b1029104bdc106c532d8d8849ba5c0f
Fred ST <frederic.soumare@gmail.com>
2022-09-07 21:19:22 +0200

    chapter 3, matrices of various sizes ought to be representable

--- matrices.test.js
+++ matrices.test.js
@@ -15,3 +15,27 @@ test('Constructing and inspecting a 4x4 matrix', () => {
   expect(M[3][0]).toBe(13.5);
   expect(M[3][2]).toBe(15.5);
 });
+
+test('A 2x2 matrix ought to be representable', () => {
+  let M = matrix(
+    [-3, 5],
+    [1, -2]
+  );
+
+  expect(M[0][0]).toBe(-3);
+  expect(M[0][1]).toBe(5);
+  expect(M[1][0]).toBe(1);
+  expect(M[1][1]).toBe(-2);
+});
+
+test('A 3x3 matrix ought to be representable', () => {
+  let M = matrix(
+    [-3, 5, 0],
+    [1, -2, -7],
+    [0, 1, 1]
+  );
+
+  expect(M[0][0]).toBe(-3);
+  expect(M[1][1]).toBe(-2);
+  expect(M[2][2]).toBe(1);
+});
