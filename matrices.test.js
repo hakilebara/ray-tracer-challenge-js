0dafd6f53257b8ff74f1bf4cccc5f3b85aca9287
Fred ST <frederic.soumare@gmail.com>
2023-02-13 18:01:38 +0100

    chapter 3, Implementing Inversion - add additional tests

--- matrices.test.js
+++ matrices.test.js
@@ -275,3 +275,50 @@ test('Calculating the inverse of a matrix', () => {
     [-0.52256, -0.81391, -0.30075,  0.30639],
   ))).toBe(true);
 });
+
+test('Calculating the inverse of another matrix', () => {
+  let A = matrix(
+    [ 8, -5,  9,  2],
+    [ 7,  5,  6,  1],
+    [-6,  0,  9,  6],
+    [-3,  0, -9, -4]
+  );
+  expect(areMatrixEqual(inverse(A), matrix(
+    [ -0.15385 , -0.15385 , -0.28205 , -0.53846 ],
+    [ -0.07692 ,  0.12308 ,  0.02564 ,  0.03077 ],
+    [  0.35897 ,  0.35897 ,  0.43590 ,  0.92308 ],
+    [ -0.69231 , -0.69231 , -0.76923 , -1.92308 ]
+  ))).toBe(true);
+});
+
+test('Calculating the inverse of a third matrix', () => {
+  let A = matrix(
+	    [  9 ,  3 ,  0 ,  9 ],
+ 	    [ -5 , -2 , -6 , -3 ],
+ 	    [ -4 ,  9 ,  6 ,  4 ],
+ 	    [ -7 ,  6 ,  6 ,  2 ]
+  );
+  expect(areMatrixEqual(inverse(A), matrix(
+	    [ -0.04074 , -0.07778 ,  0.14444 , -0.22222 ],
+ 	    [ -0.07778 ,  0.03333 ,  0.36667 , -0.33333 ],
+ 	    [ -0.02901 , -0.14630 , -0.10926 ,  0.12963 ],
+ 	    [  0.17778 ,  0.06667 , -0.26667 ,  0.33333 ]
+  ))).toBe(true);
+});
+
+test('Multiplying a prodcut by its inverse', () => {
+  let A = matrix(
+      [  3 , -9 ,  7 ,  3 ],
+      [  3 , -8 ,  2 , -9 ],
+      [ -4 ,  4 ,  4 ,  1 ],
+      [ -6 ,  5 , -1 ,  1 ]
+  );
+  let B = matrix(
+      [  8 ,  2 ,  2 ,  2 ],
+      [  3 , -1 ,  7 ,  0 ],
+      [  7 ,  0 ,  5 ,  4 ],
+      [  6 , -2 ,  0 ,  5 ]
+  );
+  let C = A.multiplyBy(B);
+  expect(areMatrixEqual(C.multiplyBy(inverse(B)), A)).toBe(true);
+});
