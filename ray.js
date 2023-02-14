d35b2ef47e0aba0708e7345a2fbffbb6364b159f
Fred ST <frederic.soumare@gmail.com>
2023-02-14 18:53:33 +0100

    chapter 5 - Ray-Sphere intersection Create sphere/ray intersect function

--- ray.js
+++ ray.js
@@ -1,3 +1,6 @@
+import { point } from './point.js';
+import { dot } from './vector.js';
+
 export class Ray {
   origin; // point
   direction; // vector
@@ -18,4 +21,24 @@ export function position(ray, t) {
   // ray's origin.
   // ray.origin + ray.direction * t
   return ray.origin.add(ray.direction.multiplyBy(t));
+}
+
+export function sphere() { return {} }
+
+export function intersects(sphere, ray) {
+  // the vector from the sphere center to the ray origin
+  // remember: the sphere is centered at the world origin
+  let sphere_to_ray = ray.origin.substract(point(0, 0, 0));
+
+  let a = dot(ray.direction, ray.direction);
+  let b = 2 * dot(ray.direction, sphere_to_ray);
+  let c = dot(sphere_to_ray, sphere_to_ray) - 1;
+  let discriminant = (b**2) - 4 * a * c;
+
+  if (discriminant < 0) return [];
+
+  let t1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  let t2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+  return t1 < t2 ? [t1, t2] : [t2, t2];
 }
